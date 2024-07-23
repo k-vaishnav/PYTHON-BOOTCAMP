e132414b1fc3edd590c288b6b1a8fb25234b1147
komal vaishnav <128462872+k-vaishnav@users.noreply.github.com>
2024-07-23 21:48:39 +0530

    Update day1_.py

--- day1_.py
+++ day1_.py
@@ -2,8 +2,8 @@
 # 1.arithmetic operators
 a=10
 b=15
-print(a+b)
-print(a-b)
-print(a//b)
-print(a*b)
-print(a%b)
+print(a+b)   """ addition"""
+print(a-b)    """subtraction"""
+print(a//b)   """division"""
+print(a*b)    """multiplication"""
+print(a%b)    """modulus"""
